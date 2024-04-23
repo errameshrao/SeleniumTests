a315f588836dcec4ce1ba9022a5acce99eadd884
Ramesh Rao <er.rameshrao@gmail.com>
2024-04-22 21:02:50 -0400

    Dataprovider added'

--- src/test/java/org/example/tests/BrowserTests.java
+++ src/test/java/org/example/tests/BrowserTests.java
@@ -7,10 +7,7 @@ import org.example.pages.HomePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class BrowserTests {
 
@@ -27,8 +24,10 @@ public class BrowserTests {
         System.out.println("Browser started");
     }
 
-    @Test
-    public void Test1() {
+    @Test(dataProvider = "signupData")
+    public void Test1(String firstName, String lastName, String email, String phoneNumber,
+                      String companyName, String jobTitle, String propertyType, String country,
+                      String whereDidYouHearAboutUs, String message) {
         HomePage homePage = new HomePage(driver);
         ContactUsPage contactUsPage = new ContactUsPage(driver);
         WebDriverUtil webDriverUtil = new WebDriverUtil();
@@ -37,9 +36,16 @@ public class BrowserTests {
         homePage.clickCompanyMenu();
         homePage.clickContactUs();
 
-        contactUsPage.submitContactUsForm("David","Lehman","david.lehman@yahoo.com","8002683750",
-                "book4time","Senior Sales Manager","Single Location Day Spa",
-                "United States","Social Media","I am looking for CRM demo");
+        contactUsPage.submitContactUsForm(firstName, lastName, email, phoneNumber, companyName, jobTitle, propertyType, country, whereDidYouHearAboutUs, message);
+    }
+
+    @DataProvider(name = "signupData")
+    public Object[][] signupData() {
+        return new Object[][]{
+                {"David", "Lehman", "david.lehman@yahoo.com", "8002683750",
+                        "book4time", "Senior Sales Manager", "Single Location Day Spa",
+                        "United States", "Social Media", "I am looking for CRM demo"}
+        };
     }
 
     @AfterClass
